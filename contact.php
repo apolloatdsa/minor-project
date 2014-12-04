8c039447af947b6951129c6b0f0d1d04f486b6ec
Thomas Delaney <thomasdelaney@webinnovate.ie>
2014-12-04 10:14:42 +0000

    Signed-off-by: Thomas Delaney <thomasdelaney@webinnovate.ie>

---
+++ contact.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/template.dwt" codeOutsideHTMLIsLocked="false" -->
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Contact Apollo</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap Core CSS -->
+<link href="bootstrap/assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="bootstrap/assets/css/shop-homepage.css" rel="stylesheet">
+<link href="bootstrap/assets/css/docs.min.css" rel="stylesheet">
+<script src="email/validation.js" type="text/javascript"></script>
+<script src="bootstrap/assets/js/custom.js" type="text/javascript"></script>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="First9col" type="boolean" value="true" -->
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="apollo.html">The Apollo team</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="carbon-meter.html">Carbon meter</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-2 ">
+                <p class="lead">Side bar nav</p>
+                <div class="list-group">
+                    <a href="section1.html" class="list-group-item">Overview</a>
+                    <a href="section2.html" class="list-group-item">Product 1</a>
+                    <a href="section3.html" class="list-group-item">Product 2</a>
+                    <a href="section4.html" class="list-group-item">Product 3</a>
+                    <a href="section5.html" class="list-group-item">Product 4</a>
+                    <a href="section6.html" class="list-group-item">Product 5</a>
+                    <a href="section7.html" class="list-group-item">Information P1</a>
+                    <a href="section8.html" class="list-group-item">Information P2</a>
+                	<a href="section9.html" class="list-group-item">Links to suppliers</a>
+                    
+                  </div>
+            </div>
+            <div class="col-md-8 col-md-offset-1">
+
+               
+
+              <div class="row"><!-- InstanceBeginEditable name="EditRegion3" -->
+                <h1>This will be the contact page</h1>
+                <p>&nbsp;</p>
+                
+                <div id="contact">
+                	<div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover">
+                          <thead>
+                            <tr>
+                              
+                              <th><span class="glyphicon glyphicon-user"></span> Name</th>
+                              <th><span class="glyphicon glyphicon-cog"></span> Stream</th>
+                              <th><span class="glyphicon glyphicon-envelope"></span> E-mail</th>
+                              <th><span class="glyphicon glyphicon-phone"></span> Phone</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              
+                              <td>Paul Grant </td>
+                              <td>Designer </td>
+                              <td><a href="mailto:paulgrant@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">pauxgrantx@webinnovate.ie</a></td>
+                              <td>087 XXXXXXX</td>
+                            </tr>
+                            <tr>
+                              
+                              <td>Patrick Mc Guinness </td>
+                              <td>Designer </td>
+                              <td><a href="mailto:patrickmcguinness@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">patrickxmcxguinnessx@webinnovate.ie</a></td>
+                              <td>086 XXXXXXX</td>
+                            </tr>
+                            <tr>
+                              
+                              <td>Niall Woods </td>
+                              <td>Designer </td>
+                              <td><a href="mailto:niallwoods@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">niallxwoodsx@webinnovate.ie</a></td>
+                              <td>087 XXXXXXX</td>
+                            </tr>
+                            <tr>
+                              
+                              <td>Thomas Delaney </td>
+                              <td>Coder</td>
+                              <td><a href="mailto:thomasdelaney@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">thomasxdelaneyx@webinnovate.ie</a></td>
+                              <td>083 XXXXXXX</td>
+                            </tr>
+                            <tr>
+                              
+                              <td>Expected soon </td>
+                              <td>Peoject manager </td>
+                              <td><a href="mailto:clairemurphy@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">clarexmurphyx@webinnovate.ie</a></td>
+                              <td>No contact yet</td>
+                            </tr>
+                            <tr>
+                              
+                              <td>AWL </td>
+                              <td>Project manager </td>
+                              <td><a href="mailto:kevinmurry@webinnovate.ie?subject=This email is an enquiry sent from the minor project website">kevinxmurryx@webinnovate.ie</a></td>
+                              <td>No contact yet</td>
+                            </tr>
+                            <tr>
+                              
+                              </tr>
+                            
+                            
+                          </tbody>
+                        </table>
+                      </div> <!-- responsive -->
+  
+ 
+  
+              </div> <!-- contact  -->
+              
+              <section class ="contact">
+              
+              	<h1>Contact Us</h1>
+                   <h3 style="color:#FF6633;"><?php echo $_GET[msg];?></h3>
+                   <hr>
+                                                                        
+                <form name="enq" method="post" action="email/" onsubmit="return validation();">
+                <fieldset>
+                    
+                <p>
+                  <input type="text" name="name" id="name" value=""  class="col-xs-8" placeholder="Name" />
+                  <input type="text" name="email" id="email" value="" class="col-xs-8" placeholder="Email" />
+                  <textarea rows="11" name="message" id="message" class="col-xs-8" placeholder="Comments"></textarea>
+                  
+                </p>
+                <div class="actions">
+                <input type="submit" value="Send Your Message" name="submit" id="submitButton" class="btn btn-info pull-right" title="Click here to submit your message!" />
+                </div>
+                    
+                </fieldset>
+                </form> 
+              
+              
+              </section>
+              
+              
+              
+              
+              
+              
+              <!-- InstanceEndEditable --></div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-11 col-lg-offset-1">
+                    <p>Copyright &copy; Digital Skills Academy &amp; The Apollo Team 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="bootstrap/assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/assets/js/bootstrap.min.js"></script>
+
+</body>
+
+<!-- InstanceEnd --></html>
