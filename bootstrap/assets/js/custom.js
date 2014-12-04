3e4836e25dd83cb1f6c2a1cebd3cda27af0c08ad
Thomas Delaney <thomasdelaney@webinnovate.ie>
2014-12-04 10:56:46 +0000

    Signed-off-by: Thomas Delaney

--- bootstrap/assets/js/custom.js
+++ bootstrap/assets/js/custom.js
@@ -5,50 +5,50 @@
  								
 								
 								
-								function setM(v){ //v is a value supplied by the calling function -- a variable called meter 
-									
-									document.getElementById('carbonM').meterValue.value = v; // take the value of v and write it to the number input
-									document.getElementById('carbonM').range.value = v; // take the value of v and write it to the slider input
-									document.getElementById("meter").src = "bootstrap/assets/img/"+v+"-meter.jpg"; // change the image
-									
-									
-									};
+	function setM(v){ //v is a value supplied by the calling function -- a variable called meter 
+										
+	document.getElementById('carbonM').meterValue.value = v; // take the value of v and write it to the number input
+	document.getElementById('carbonM').range.value = v; // take the value of v and write it to the slider input
+	document.getElementById("meter").src = "bootstrap/assets/img/"+v+"-meter.jpg"; // change the image
+										
+										
+	};
 								
-								function readMeter(i){  // i is to identify the i/p either the slider i/p or the number i/p calling the function
+	function readMeter(i){  // i is to identify the i/p either the slider i/p or the number i/p calling the function
 								
-									meter = document.getElementById('carbonM').range.value; // create variable meter by reading value from slider
+	meter = document.getElementById('carbonM').range.value; // create variable meter by reading value from slider
 									
-									if(i == "number"){
-									meter = document.getElementById('carbonM').meterValue.value; // get value entered in meter value box
-									}
+	if(i == "number"){
+	meter = document.getElementById('carbonM').meterValue.value; // get value entered in meter value box
+	}
 									
-									setM(meter)
-									}
+	setM(meter)
+		}
 					
 								
-								function increaseMeter(){
+		function increaseMeter(){
 									
-									if(document.getElementById('carbonM').range.value <=14){
+		if(document.getElementById('carbonM').range.value <=14){
 									
-									meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
-									meter++;
-									setM(meter)
-									}
-									
-									
-									}	
-									
-									function decreaseMeter(){
-									
-									if(document.getElementById('carbonM').range.value >0){
-									
-									meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
-									meter--;
-									setM(meter)
-									}
+		meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
+		meter++;
+		setM(meter)
+		}
 									
 									
-									}	
+		}	
+									
+		function decreaseMeter(){
+									
+		if(document.getElementById('carbonM').range.value >0){
+									
+		meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
+		meter--;
+		setM(meter)
+		}
+									
+									
+		}	
 									
 		
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
