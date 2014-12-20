9e1d02261655e04bb9e93fb45286aa4194da4d3c
Thomas Delaney <thomasdelaney@webinnovate.ie>
2014-12-20 12:17:47 +0000

    Added logo and banner
    added banner and logo

--- js/custom.js
+++ js/custom.js
@@ -13,52 +13,96 @@
  								
 								
 								
-								function setM(v){ //v is a value supplied by the calling function -- a variable called meter (0 - 15)
+	function setM(v){ //v is a value supplied by the calling function -- a variable called meter (0 - 15)
 									
-									document.getElementById('carbonM').meterValue.value = v; // take the value of v and write it to the number input
-									document.getElementById('carbonM').range.value = v; // take the value of v and write it to the slider input
-									document.getElementById("meter").src = "img/"+v+"-meter.jpg"; // change the image to show position (0 - 15)
+	document.getElementById('carbonM').meterValue.value = v; // take the value of v and write it to the number input
+	document.getElementById('carbonM').range.value = v; // take the value of v and write it to the slider input
+	document.getElementById("meter").src = "img/"+v+"-meter.jpg"; // change the image to show position (0 - 15)
 									
 									
-									};
+		};
 								
-								function readMeter(i){  // i is to identify the i/p either the slider i/p or the number i/p calling the function
+	function readMeter(i){  // i is to identify the i/p either the slider i/p or the number i/p calling the function
 								
-									meter = document.getElementById('carbonM').range.value; // create variable meter by reading value from slider
+	meter = document.getElementById('carbonM').range.value; // create variable meter by reading value from slider
 									
-									if(i == "number"){
-									meter = document.getElementById('carbonM').meterValue.value; // get value entered in meter value box
-									}
+	if(i == "number"){
+	meter = document.getElementById('carbonM').meterValue.value; // get value entered in meter value box
+	}
 									
-									setM(meter)
-									}
+	setM(meter)
+		}
 					
 								
-								function increaseMeter(){
+	function increaseMeter(){
 									
-									if(document.getElementById('carbonM').range.value <=14){
+	if(document.getElementById('carbonM').range.value <=14){
 									
-									meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
-									meter++;
-									setM(meter)
-									}
-									
-									
-									}	
-									
-									function decreaseMeter(){
-									
-									if(document.getElementById('carbonM').range.value >0){
-									
-									meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
-									meter--;
-									setM(meter)
-									}
+	meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
+	meter++;
+	setM(meter)
+		}
 									
 									
-									}	
+	}	
+									
+	function decreaseMeter(){
+									
+	if(document.getElementById('carbonM').range.value >0){
+									
+	meter = document.getElementById('carbonM').range.value; // get value entered in meter value box
+	meter--;
+	setM(meter)
+	}
+									
+									
+	}	
 									
 		
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////									
+	function on_load_login(){
+
 	
+    var loginButton = document.getElementById("login-btn");// event listener for range slider
+	 
+	 if(loginButton.addEventListener){
+         loginButton.addEventListener("click", readLogin );
+   
+   };
+	
+	};
+	
+	
+	
+	
+//////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+var loginEmail;
+var loginUser =' Please login ';
+var localUser = localStorage.getItem("user");
+
+ function readLogin(){
+	 
+	 loginEmail = document.getElementById("login-em").value;
+	 var loginUser = document.getElementById("login-user").value;
+	 localStorage.setItem("user", loginUser);
+	 document.getElementById("logged-in-user").innerHTML = "Welcome "+loginUser;
+	 
+	 alert(foo)
+	 //$('login').modal('hide');
+	 
+	 
+	 //alert("Login email is "+loginEmail+"\n Login User is "+loginUser);
+	 
+	 }	
+	 
+ function userDiv() {
+	 //loginUser = 'Please login';
+	 
+	 localUser = localStorage.getItem("user");
+	 //alert("This the User div function "+foo);
+	 document.getElementById("logged-in-user").innerHTML = "Welcome "+localUser;
+	 
+	 }
